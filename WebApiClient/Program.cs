d44d3fa39cbef87998b5ba13a08b45160b3b50b7
BattlePatata <108839534+BattlePatata@users.noreply.github.com>
2024-02-21 17:39:49 +0500

    Add files via upload

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+ 
+static async Task Main(string[] args) {
+    Console.WriteLine("press any key to countinue...");
+    Console.ReadLine();
+
+    using (HttpClient client = new HttpClient())
+    {
+        var response = await client.GetAsync("http://localhost:5063/values");
+        response.EnsureSuccessStatusCode();
+        if (response.IsSuccessStatusCode)
+        {
+            string message = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine($"response error code: {response.StatusCode}");
+        }
+    }
+
+    Console.ReadLine();
+    Console.WriteLine("Hello, World!");
+}
+
+await Main(args);
